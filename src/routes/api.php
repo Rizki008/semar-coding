1574e36409f126194eede496475e017d23d23c46
Rizki008 <muhammadrizkinurhamdani@gemail.com>
2024-11-05 22:10:46 +0700

    menambahkan api list product

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ListproductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,11 @@ Route::group(
         Route::post('login', [AuthController::class, 'login'])->name('login');
     }
 );
+
+Route::group([
+    'middleware' => 'api'
+], function () {
+    Route::resources([
+        'listproducts' => ListproductController::class,
+    ]);
+});
