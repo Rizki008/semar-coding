9e64bfbd3388449adea0922c9d4c0f29630c643e
Rizki008 <muhammadrizkinurhamdani@gemail.com>
2024-11-09 11:45:00 +0700

    menambahkan api transaksi

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ListproductController;
+use App\Http\Controllers\TransaksiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +23,13 @@ Route::group([
     Route::resources([
         'listproducts' => ListproductController::class,
         'clients' => ClientController::class,
+        'transaksis' => TransaksiController::class,
     ]);
+
+    Route::get('transaksi/dikonfirmasi', [TransaksiController::class, 'dikonfirmasi']);
+    Route::get('transaksi/dikemas', [TransaksiController::class, 'dikemas']);
+    Route::get('transaksi/dikirim', [TransaksiController::class, 'dikirim']);
+    Route::get('transaksi/diterima', [TransaksiController::class, 'diterima']);
+    Route::get('transaksi/selesai', [TransaksiController::class, 'selesai']);
+    Route::post('transaksi/ubah_status/{transaksi}', [TransaksiController::class, 'ubah_status']);
 });
